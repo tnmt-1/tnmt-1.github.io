7c9f248a379ff01ea6edaf928415fadf0b5165da
mtanimoto <masashi_69@live.jp>
2022-06-06 20:58:32 +0900

    opengraphの修正とconsole.logの削除

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,4 @@
-import { NextSeo } from 'next-seo'
+import { NextSeo, DefaultSeo } from 'next-seo'
 import striptags from "striptags";
 import { posts, post } from '../../lib/posts';
 
@@ -8,6 +8,14 @@ export default function BlogId({ blog }) {
   return (
     <main>
       <NextSeo title={`${blog.title}`} description={description} />
+      <DefaultSeo
+        openGraph={{
+          type: 'website',
+          title: blog.title,
+          description: description,
+          url: `https://mt2m10.github.io/posts/${blog.id}`,
+        }}
+      />
       <h1>{blog.title}</h1>
       <small><time datestring={blog.publishedAt}>{blog.publishedAt}</time></small>
       <article
@@ -22,14 +30,13 @@ export default function BlogId({ blog }) {
 // 静的生成のためのパスを指定します
 export const getStaticPaths = async () => {
   const data = await posts();
-  console.log(data);
   const paths = data.map((content) => `/posts/${content.id}`);
   return { paths, fallback: false };
 };
 
 // データをテンプレートに受け渡す部分の処理を記述します
 export const getStaticProps = async (context) => {
-  const data = await post(context.params.id + '.md');
+  const data = await post(context.params.id);
 
   return {
     props: {
